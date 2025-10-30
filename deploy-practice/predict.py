aa4d348b1ed1ac0bfd569a9d53dd064253ea5c6c
Stephen Adegbokun <obaadegbokun1@gmail.com>
2025-10-30 22:28:39 +0000

    deployment

---
+++ deploy-practice/predict.py
@@ -0,0 +1,33 @@
+# sample datapoint 
+import requests
+
+url = "http://localhost:9696/predict"
+# url = "https://sparkling-lake-3705.fly.dev/predict"
+
+datapoint = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+res = requests.post(url, json=datapoint)
+
+predictions = res.json()
+
+print(f"Prob of Churn: {predictions['churn_probability']}")
